48ae0610d5952093379fd28938b4f1fe0df6580e
James Leslie <contactjamesleslie@gmail.com>
2022-08-03 15:41:34 +1200

    Used the FplApiData class

--- src/load_data.py
+++ src/load_data.py
@@ -1,22 +1,23 @@
 import pandas as pd
 import streamlit as st
-import time
+
+from datasets import FplApiData
 
 
-@st.cache
+@st.cache  # only hits the API once and stores the result in memory
 def get_fpl_data():
 
-    data_url_base = 'https://raw.githubusercontent.com/datahounds/fantasy-premier-league/master/data/2022-23/'
+    api_data = FplApiData()
+    players = api_data.players
+    positions = api_data.positions
+    teams = api_data.teams
 
-    players = pd.read_csv(data_url_base + 'players.csv')
-    positions = pd.read_csv(data_url_base + 'positions.csv')
-    teams = pd.read_csv(data_url_base + 'teams.csv')
+    df = players.merge(teams, on='team_id').merge(positions, on='position_id').reset_index()
 
-    df = players.merge(teams, on='team_id').merge(positions, on='position_id')
     df = df[df['minutes']>270]
 
     df = df[
-        ['player_id', 'web_name', 'short_name', 'position_name', 'now_cost',
+        ['web_name', 'short_name', 'position_name', 'now_cost',
         'total_points', 'points_per_game', 'minutes', 'goals_scored',
         'penalties_missed', 'assists', 'yellow_cards', 'red_cards', 'clean_sheets',
         'goals_conceded', 'saves', 'own_goals','penalties_saved', 'bonus', 'bps',
@@ -25,7 +26,7 @@ def get_fpl_data():
         'total_points', ascending=False
     )
 
-    df.columns = ['id', 'name', 'team', 'pos', 'price', 'P', 'PPG', 'M', 'GS', 'PM',
+    df.columns = ['name', 'team', 'pos', 'price', 'P', 'PPG', 'M', 'GS', 'PM',
                 'A', 'YC', 'RC', 'CS', 'GC', 'S', 'OG', 'PS', 'B', 'BPS', 'I',
                 'C', 'T', 'ICT']
 
@@ -37,8 +38,5 @@ def get_fpl_data():
 
     # add points per 90 column
     df['PP90'] = df['P'] / df['M'] * 90
-
-    # testing out the spinner functionality
-    time.sleep(2)
     
     return players, positions, teams, df
