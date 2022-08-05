4584ee9382f83e1cd98402ad1809703e5a800a39
James Leslie <contactjamesleslie@gmail.com>
2022-08-05 17:00:21 +1200

    Changed to 22-23 data

--- app/Home.py
+++ app/Home.py
@@ -6,16 +6,22 @@ import streamlit as st
 from load_data import FplData
 
 
+def display_frame(df):
+    '''display dataframe with all float columns rounded to 1 decimal place'''
+    float_cols = df.select_dtypes(include='float64').columns.values
+    st.dataframe(df.style.format(subset=float_cols, formatter='{:.1f}'))
+
+
 st.set_page_config(page_title='FPL dashboard', page_icon='⚽', layout='wide')
 
 # load data from github
 with st.spinner():
     fpl_data = FplData()
-    st.session_state['summary'] = fpl_data.get_summary()
     st.session_state['data'] = fpl_data
 
-players = st.session_state['data'].players
-df = st.session_state['summary']
+df = st.session_state['data'].df_total
+df_90 = st.session_state['data'].df_90
+df_gp = st.session_state['data'].df_gp
 
 # -------------------------------------------------------------------- side bar
 # user id input
@@ -24,59 +30,72 @@ st.sidebar.write('Your team id:', st.session_state.fpl_id)
 
 position_select = st.sidebar.multiselect(
     'Position',
-    players['pos'].unique()
+    df['pos'].unique()
 )
 team_select = st.sidebar.multiselect(
     'Team',
-    players['team'].unique()
+    df['team'].unique()
 )
 price_max = st.sidebar.selectbox(
     'Max price',
     np.arange(13.5, 3.5, -0.5)
 )
 if team_select:
-    df = df[df['team'].isin(team_select)]
+    team_filter = df['team'].isin(team_select)
+    df = df[team_filter]
+    df_90 = df_90[team_filter]
+    df_gp = df_gp[team_filter]
 if position_select:
-    df = df[df['pos'].isin(position_select)]
+    pos_filter = df['pos'].isin(position_select)
+    df = df[pos_filter]
+    df_90 = df_90[pos_filter]
+    df_gp = df_gp[pos_filter]
 if price_max:
     df = df[df['£'] <= price_max]
 
 # --------------------------------------------------------------- main container
-# show table of players
-# format float columns to 1 decimal place
+# ---------------------------------------------------- dataframes
 st.header('Player summary')
-float_cols = df.select_dtypes(include='float64').columns.values
-st.dataframe(df.style.format(subset=float_cols, formatter='{:.1f}'))
 
+# info_cols = ['name', 'team', 'pos', '£', 'GP', 'MP']
+# score_total_cols = ['Pts', 'GS', 'A', 'CS', 'B', 'BPS', 'I', 'C', 'T', 'II']
+# score_90_cols = [c + '/90' for c in score_total_cols]
+# score_gp_cols = [c + '/GP' for c in score_total_cols]
+
+st.subheader('Season totals')
+display_frame(df)
+
+st.subheader('Totals per 90 minutes')
+display_frame(df_90)
+
+st.subheader('Totals per game played')
+display_frame(df_gp)
+
+# ------------------------------------------------- scatter plots
 scatter_x_var = st.selectbox(
     'X axis variable',
-    ['Influence', 'Creativity', 'Threat', 'ICT Index']
+    ['ICT Index', 'Influence', 'Creativity', 'Threat']
 )
-scatter_90_lookup = {
-    'Influence': 'I/90', 'Creativity': 'C/90', 'Threat': 'T/90',
-    'ICT Index': 'II/90'
-}
-scatter_GP_lookup = {
-    'Influence': 'I/GP', 'Creativity': 'C/GP', 'Threat': 'T/GP',
-    'ICT Index': 'II/GP'
+scatter_lookup = {
+    'Influence': 'I', 'Creativity': 'C', 'Threat': 'T', 'ICT Index': 'II'
 }
 
 col1, col2 = st.columns(2)
 with col1:
     st.header('Points per 90')
-    c = alt.Chart(df).mark_circle(size=75).encode(
-        x=scatter_90_lookup[scatter_x_var],
-        y='Pts/90',
+    c = alt.Chart(df_90).mark_circle(size=75).encode(
+        x=scatter_lookup[scatter_x_var],
+        y='Pts',
         color='pos',
-        tooltip=['name', 'Pts/90']
+        tooltip=['name', 'Pts']
     )
     st.altair_chart(c, use_container_width=True)
 with col2:
     st.header('Points per game played')
-    c = alt.Chart(df).mark_circle(size=75).encode(
-        x=scatter_GP_lookup[scatter_x_var],
-        y='Pts/GP',
+    c = alt.Chart(df_gp).mark_circle(size=75).encode(
+        x=scatter_lookup[scatter_x_var],
+        y='Pts',
         color='pos',
-        tooltip=['name', 'Pts/GP']
+        tooltip=['name', 'Pts']
     )
     st.altair_chart(c, use_container_width=True)
--- app/pages/1_👕_Player_view.py
+++ app/pages/1_👕_Player_view.py
@@ -4,37 +4,36 @@ import streamlit as st
 
 st.set_page_config(page_title='FPL Player Details', page_icon='👕', layout='wide')
 
-players = st.session_state['data'].players
-history = st.session_state['data'].history
-df = st.session_state['summary']
+df = st.session_state['data'].df_total
+df_90 = st.session_state['data'].df_90
+df_gp = st.session_state['data'].df_gp
 
 # -------------------------------------------------------------------- side bar
 # position slicer
 position_select = st.sidebar.multiselect(
     'Position',
-    players['pos'].unique()
+    df['pos'].unique()
 )
 if position_select:
-    players = players[players['pos'].isin(position_select)]
     df = df[df['pos'].isin(position_select)]
 # team slicer
 team_select = st.sidebar.multiselect(
     'Team',
-    players['team'].unique()
+    df['team'].unique()
 )
 if team_select:
-    players = players[players['team'].isin(team_select)]
+    df = df[df['team'].isin(team_select)]
 # price slicer
 price_max = st.sidebar.selectbox(
     'Max price',
     np.arange(13.5, 3.5, -0.5)
 )
 if price_max:
-    players = players[players['£'] <= price_max]
+    df = df[df['£'] <= price_max]
 # player selector
 player_select = st.sidebar.radio(
     'Player',
-    players['name'].unique()
+    df['name'].unique()
 )
 
 # -------------------------------------------------------------------- metrics
@@ -50,5 +49,4 @@ st.write(gs_player)
 
 st.metric('Goals scored', gs_player, delta=f'{gs_player-gs_pos_avg:.2f}')
 
-players[players['name']==player_select]
-history
+df[df['name']==player_select]
