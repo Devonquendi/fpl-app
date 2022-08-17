9fd613f5dd981e6133152693407950da9f12d5d5
James Leslie <contactjamesleslie@gmail.com>
2022-08-17 14:58:50 +1200

    Added gameweek range slider

--- app/pages/2_📆_Fixtures.py
+++ app/pages/2_📆_Fixtures.py
@@ -17,13 +17,37 @@ fpl_data = st.session_state['data']
 gameweeks = st.session_state['data'].gameweeks
 teams = st.session_state['data'].teams
 
-fixture_ratings, fixture_team_ids, fixture_team_names = fpl_data.get_fixtures()
+# get next gameweek number from gameweeks df
+NEXT_GW = gameweeks[gameweeks['is_next']].index.tolist()[0]
+
+# --------------------------------------------------------------------- side bar
+# position slicer
+gw_slider = st.sidebar.slider(
+    label='Gameweek range',
+    min_value=int(gameweeks.index.min()),
+    max_value=int(gameweeks.index.max()),
+    value=(NEXT_GW, NEXT_GW+10)
+)
+
+start_gw, end_gw = gw_slider
+
+st.write('Gameweeks:', gw_slider)
+
+# get fixtures between chosen gameweek range
+fixture_ratings, fixture_team_ids, fixture_team_names = fpl_data.get_fixtures(start_gw, end_gw)
+
+# --------------------------------------------------------------- main container
 
 st.subheader('Teams')
 teams
 st.subheader('Gameweeks')
 gameweeks
+gameweeks[gameweeks['is_next']]
+NEXT_GW
 st.subheader('Fixtures')
-fixture_ratings
+st.write('Team ids')
 fixture_team_ids
+st.write('Team ratings')
+fixture_ratings
+st.write('Team names')
 fixture_team_names
