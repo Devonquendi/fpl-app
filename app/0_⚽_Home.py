a5724a691a803a499dda6a9e449d8e656a0b0f6e
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-07-21 12:42:37 +1200

    Added xGI / GI scatter plot with trendlines to the Home page

--- app/0_⚽_Home.py
+++ app/0_⚽_Home.py
@@ -1,8 +1,26 @@
-import numpy as np
 import streamlit as st
-from st_helpers import load_data, display_frame
-
+from st_helpers import load_data
+import plotly.express as px
 
 st.set_page_config(
     page_title='FPL dashboard', page_icon='⚽', layout='wide')
 
+fpl_data = load_data()
+
+players = fpl_data.players_df
+
+st.title('FPL Dashboard')
+
+st.write('This is some placeholder text description of the FPL Dashboard')
+
+fig = px.scatter(
+    players,
+    x="GI",
+    y="xGI",
+    size="£",
+    color="pos",
+    hover_name="player_name",
+    trendline="ols"
+)
+
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
