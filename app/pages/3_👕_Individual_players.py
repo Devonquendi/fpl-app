350fcc35c93f11b37f05efad21ff5f519f1d8ce8
James Leslie <James.Leslie@at.govt.nz>
2024-09-03 19:42:29 +1200

    Added plots

--- app/pages/3_👕_Individual_players.py
+++ app/pages/3_👕_Individual_players.py
@@ -1,11 +1,10 @@
 import numpy as np
+import plotly.express as px
 import streamlit as st
-from utils import load_data, donate_message
 from styles import style_background_player_fdr
+from utils import donate_message, load_data
 
-
-st.set_page_config(
-    page_title='FPL Player Details', page_icon='👕', layout='wide')
+st.set_page_config(page_title="FPL Player Details", page_icon="👕", layout="wide")
 
 
 # load data from API
@@ -13,76 +12,81 @@ fpl_data = load_data()
 
 gameweeks = fpl_data.gameweeks_df
 teams = fpl_data.teams_df
-players = fpl_data.players_df.sort_values(
-    'Pts', ascending=False)
+players = fpl_data.players_df.sort_values("Pts", ascending=False)
 
 # -------------------------------------------------------------------- side bar
 # position slicer
-position_select = st.sidebar.multiselect(
-    'Position',
-    players['pos'].unique()
-)
+position_select = st.sidebar.multiselect("Position", players["pos"].unique())
 if position_select:
-    players = players[players['pos'].isin(position_select)]
+    players = players[players["pos"].isin(position_select)]
 # team slicer
-team_select = st.sidebar.multiselect(
-    'Team',
-    players['team'].unique()
-)
+team_select = st.sidebar.multiselect("Team", players["team"].unique())
 if team_select:
-    players = players[players['team'].isin(team_select)]
+    players = players[players["team"].isin(team_select)]
 # price slicer
-price_max = st.sidebar.selectbox(
-    'Max price',
-    np.arange(14.5, 3.5, -0.5)
-)
+price_max = st.sidebar.selectbox("Max price", np.arange(14.5, 3.5, -0.5))
 if price_max:
-    players = players[players['£'] <= price_max]
+    players = players[players["£"] <= price_max]
 # player selector
-player_select = st.sidebar.radio(
-    'Player',
-    players['player_name'].values
-)
-selected_player_id = players[
-    players['player_name'] == player_select].index.tolist()[0]
+player_select = st.sidebar.radio("Player", players["player_name"].values)
+selected_player_id = players[players["player_name"] == player_select].index.tolist()[0]
+
+player_history = fpl_data.get_player_summary(selected_player_id, type="history")
 
 player_history_past = fpl_data.get_player_summary(
-    selected_player_id, type='history_past')
+    selected_player_id, type="history_past"
+)
 
 # ---------------------------------------------------------------main container
 player_info = players.loc[selected_player_id].to_dict()
-player_name = player_info['first_name'] + ' ' + player_info['second_name']
-player_pos = player_info['pos']
-player_team = player_info['team']
+player_name = player_info["first_name"] + " " + player_info["second_name"]
+player_pos = player_info["pos"]
+player_team = player_info["team"]
 
 st.header(player_name)
-st.subheader(f'{player_pos} - {player_team}')
+st.subheader(f"{player_pos} - {player_team}")
 
 # ------------------------------------ metrics
-st.subheader('Season totals')
+st.subheader("Season totals")
 player_totals = players.loc[selected_player_id].to_dict()
 
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
-    st.metric(label='Price', value=f"£{player_totals['£']}")
+    st.metric(label="Price", value=f"£{player_totals['£']}")
 with col2:
-    st.metric(label='PPG', value=player_totals['PPG'])
+    st.metric(label="PPG", value=player_totals["PPG"])
 with col3:
-    st.metric(label='xGI90', value=player_totals['xGI90'])
+    st.metric(label="xGI90", value=player_totals["xGI90"])
 with col4:
-    st.metric(label='xGC90', value=player_totals['xGC90'])
+    st.metric(label="xGC90", value=player_totals["xGC90"])
 with col5:
-    st.metric(label='TSB', value=f"{player_totals['TSB%']}%")
+    st.metric(label="TSB", value=f"{player_totals['TSB%']}%")
+
+# ------------------------------------ past performance
+st.subheader("Past performance")
+st.dataframe(player_history.sort_index(ascending=False), use_container_width=True)
+
+# plot cummulative xGI
+fig = px.line(
+    player_history[["xG", "GS"]].cumsum(),
+    range_y=[0, player_history[["xG", "GS"]].cumsum().max().max() + 1],
+)
+st.plotly_chart(fig)
+
+fig = px.line(
+    player_history,
+    y=["xG", "GS"],
+    range_y=[0, player_history[["xG", "GS"]].max().max() + 1],
+)
+st.plotly_chart(fig)
 
 # ------------------------------------ upcoming fixtures
-st.subheader('Upcoming fixtures')
+st.subheader("Upcoming fixtures")
 
-player_fixtures = fpl_data.get_player_summary(
-    selected_player_id, 'fixtures')
+player_fixtures = fpl_data.get_player_summary(selected_player_id, "fixtures")
 
 st.dataframe(
-    player_fixtures.T.style.map(style_background_player_fdr),
-    use_container_width=True
+    player_fixtures.T.style.map(style_background_player_fdr), use_container_width=True
 )
 
 donate_message()
