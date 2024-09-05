2c74e2a26c7780cd9bfd3d357fd819a0f88eba2f
James Leslie <contactjamesleslie@gmail.com>
2024-09-05 22:59:16 +0000

    Added google form link

--- app/0_⚽_Home.py
+++ app/0_⚽_Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="FPLstat: Home", page_icon="⚽", layout="wide")
 
@@ -19,3 +20,5 @@ st.markdown(
 If you have any feedback or suggestions, feel free to send me an email at 
 contactjamesleslie@gmail.com"""
 )
+
+components.iframe('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdxqBjzjBtQxE4lcHp0LsORBTr6O1_kq4tvUwlZqF8l3u-SXA/viewform?embedded=true" width="640" height="737" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>', height=500)
